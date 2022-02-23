34673b34e05b20a824504fca17e08c161a6e6062
Acer E15 <jamesvs2020@gmail.com>
2022-02-23 01:57:05 -0600

    home page appearance

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -20,6 +20,7 @@ function Home() {
   return <div className='home-page'>
     <div className='banner-container'>
       <div className='banner-imgs'>
+        <h1 className='banner-txt'>Go out and explore</h1>
         <Carousel pause={false} controls={false} className='home-carousel' fade>
             {imgs.map(img => {
                 return(
@@ -30,20 +31,67 @@ function Home() {
                     alt="First slide"
                     />
                     <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>Whats Poppin</h3>
+                    <p>Find events in your area. Meet people. Make Memories.</p>
                     </Carousel.Caption>
                 </Carousel.Item>)
             })}
         </Carousel>
+        <div className='homeRegisterBox'>
+            <button className='homeRegister-btn'>Log In</button>
+            <button className='homeRegister-btn'>Sign Up</button>
+            <button className='homeRegister-btn'>Find Events</button>
+        </div>
       </div>
     </div>
     
     
 
-    <div className='home-container1'>
-      <h1>Upcoming Events</h1>
+    <div className='home-container-r2'>
+      <div className='home-content-r2-c1'>
+        <div className='r2c1-other-panel'></div>
+        <img className='home-content-c1-img' src='/pageImages/small/gettyimages-855383198-612x612.jpg' alt='city' />
+        <div className='r2c1-text-panel'>
+          <div className='r2c1-text'>
+            <h2>Find Events Near You</h2>
+            <p>Traveling to a new city and looking for what to do? Or just looking to find something new in your city? Whats Poppin has all the events you're looking for.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='home-content-r2-c2'>
+        <div className='r2c2-other-panel'></div>
+        <div className='r2c2-text-panel'>
+          <h2>Dicover New Places</h2>
+          <p>With friends</p>
+        </div>
+          <img className='home-content-c2-img' src='/pageImages/small/gettyimages-1159841835-612x612.jpg' alt='carnival game' />
+      </div>
+
+      <div className='home-content-r2-c2'>
+        <div className='r2c2-other-panel'></div>
+        <div className='r2c2-text-panel'>
+          <h2>Dicover New Places</h2>
+          <p>With friends</p>
+        </div>
+          <img className='home-content-c2-img' src='/pageImages/small/gettyimages-1148769679-612x612.jpg' height="360px" alt='carnival game' />
+      </div>
+
+      <div className='home-content-r2-c1'>
+        <div className='r2c1-other-panel'></div>
+        <img className='home-content-c1-img' src='/pageImages/small/gettyimages-1157908388-612x612.jpg' height="360px" alt='city' />
+        <div className='r2c1-text-panel'>
+          <div className='r2c1-text'>
+            <h2>Find Events Near You</h2>
+            <p>Traveling to a new city and looking for what to do? Or just looking to find something new in your city? Whats Poppin has all the events you're looking for.</p>
+          </div>
+        </div>
+      </div>
     </div>  
+
+    <div className='home-container-r3'>
+            <h4 className='pageTitle'>Copyright © 2022 Whats Poppin</h4>
+    </div>
   </div>;
 }
 
